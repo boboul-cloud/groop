8169d9e08bc40f13dbc722f5f08103737c4e1162
BudgetApp <bob.oulhen@gmail.com>
2026-03-11 12:23:36 +0100

    Initial Commit

---
+++ coquilles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  coquilles
+//
+//  Created by Robert Oulhen on 11/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ coquilles/coquillesApp.swift
@@ -0,0 +1,17 @@
+//
+//  coquillesApp.swift
+//  coquilles
+//
+//  Created by Robert Oulhen on 11/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct coquillesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
